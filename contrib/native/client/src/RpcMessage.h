5ff198aa3dfa2ef6604a1103e08a8e71643f3620
Xiao Meng <xiaom.cs@gmail.com>
2014-02-19 23:17:29 -0800

    initial commit of protype for native drill C++ client

---
+++ contrib/native/client/src/RpcMessage.h
@@ -0,0 +1,67 @@
+#ifndef RPC_MESSAGE_H
+#define RPC_MESSAGE_H
+#include <google/protobuf/message_lite.h>
+#include "common.h"
+using google::protobuf::MessageLite;
+using exec::rpc::RpcMode;
+
+
+
+class InBoundRpcMessage {
+
+public:
+    RpcMode m_mode;
+    int m_rpc_type;
+    int m_coord_id;
+    DataBuf m_pbody;
+    friend ostream& operator << (ostream & out, InBoundRpcMessage& msg);
+    DataBuf m_dbody;
+
+    /*
+    InBoundRpcMessage(RpcMode mode,
+    	int rpc_type, int coord_id, DataBuf pbody, vector<DataBuf>  dbody):
+        m_mode(mode), m_rpc_type(rpc_type), m_coord_id(coord_id), m_pbody(pbody), m_dbody(dbody) { }
+    */
+
+    // GetBodySize
+    // ToString
+    // get ProtobufBodyAsIs
+
+};
+
+class OutBoundRpcMessage {
+public:
+
+    RpcMode m_mode;
+    int m_rpc_type;
+    int m_coord_id;
+    const MessageLite* m_pbody;
+
+    OutBoundRpcMessage(RpcMode mode, int rpc_type, int coord_id, const MessageLite* pbody):
+        m_mode(mode), m_rpc_type(rpc_type), m_coord_id(coord_id), m_pbody(pbody) { }
+
+    friend ostream& operator << (ostream & out, OutBoundRpcMessage& msg);
+    // GetBodySize
+    // GetRawBodySize
+    // ToString
+};
+
+ostream& operator<< (ostream & out, InBoundRpcMessage & msg)
+{
+
+    out << "InboundRpcMessage [pBody= (" << msg.m_pbody.size() << " bytes)"  \
+        << ", mode=" << msg.m_mode  << ", rpcType=" << msg.m_rpc_type  << ", coordinationId=" << msg.m_coord_id \
+        << ", dBodies= (" << msg.m_dbody.size() << " bytes)" \
+        << "]" ;
+    return out;
+}
+ostream& operator<< (ostream & out, OutBoundRpcMessage & msg)
+{
+    out << "OutboundRpcMessage [pBody= (" << msg.m_pbody->ByteSize() << " bytes)"\
+        << ", mode=" << msg.m_mode  << ", rpcType=" << msg.m_rpc_type  << ", coordinationId=" << msg.m_coord_id \
+        // << ", dBodies=" << m_dBodies
+        << "]";
+    return out;
+}
+
+#endif
---
+++ contrib/native/client/src/cluster_coordinator.h
@@ -0,0 +1,18 @@
+#ifndef _CLUSTER_COORDINATOR_H
+#define _CLUSTER_COORDINATOR_H
+
+
+class ClusterCoordinator{
+public:
+    // TODO
+    // connect to zookeeper, timeout mills
+    bool Start(uint64_t mills){
+        return true;
+    }
+
+    // TODO
+    // get available endpoints
+    vector<UserServerEndPoint> GetAvailableEndpoints();
+}
+
+#endif
