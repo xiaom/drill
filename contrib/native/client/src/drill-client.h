91444a9525c667487874ad07f7d583d665d3cea7
Xiao Meng <xiaom.cs@gmail.com>
2014-02-25 11:56:32 -0800

    Update drill-client.h

--- contrib/native/client/src/drill-client.h
+++ contrib/native/client/src/drill-client.h
@@ -1,10 +1,6 @@
 #ifndef DRILL_CLIENT_H
 #define DRILL_CLIENT_H
 #include "common.h"
-#include "rpc-encoder.h"
-#include "rpc-decoder.h"
-#include "rpc-message.h"
-
 
 namespace Drill {
 
@@ -13,119 +9,93 @@ struct DrillConfig {
     int m_reconnect_times;
 };
 
-// a wrapper for the response
-struct ExecutationContext {
+// Wrapper for response of each request
+struct ExecutionContext {
     bool m_coord_id;                    // the coordination id for each request
-    exec::user::QueryResult m_result;
+	exec::user::QueryResult m_result;   // including query_id, record count and so on 
     bool m_failure;                     // whether it is a request failure
     exec::rpc::RpcFailure m_failure;    // the failure message if we get failed
 };
 
-// Consider using asio::strand for async I/O
+class ClusterCoordinator {
+	/// @brief connect to a quorum of zookeeper 
+	virtual void Connect() = 0 ;
+	/// @brief refresh the cluster 
+	virtual void Refresh() = 0 ;
+	/// @brief get an available drillbit
+	virtual void GetQueryCoordinator(UserServerEndPoint& endpoint) = 0;
+	/// @brief close the connection to the quorum
+	virtual void Close() = 0;
+};
+
+class RecordBatchBuffer{
+	vector<RecordBatch> m_buffer;
+	
+	/// Provide functions to iterate recordbatch, convert to list of strings and so on
+};
+
+
 class DrillClient {
 
   public:
-    virtual ~DrillClienit() = 0;
+  
+	virtual ~DrillClient() = 0;
     
-    /// connect to a zookeepr and ask for coordination 
-    virtual void ZKConnect() = 0 ;
-    /// get available drillbit
-    virtual void ZKGetAvailableDrillbit(UserServerEndPoint& endpoint) = 0;
-    /// close the connection with zookeeper
-    virtual void ZKClose() = 0;
-
-
-    /// @brief open a session to the user server endpoint coordinated by Drill Client
+    /// @brief Open a session to the user server endpoint coordinated by quorum
     /// 
-    /// It will send a initial handshake request and validate the handshake.
-    virtual bool OpenSession(ExecutationContext& context) = 0; 
+    /// It will send an initial handshake request and validate the handshake.
+	/// The reconnection should also been done by 
+	/// 
+	/// @param[in]  quorum
+	/// @param[out] context
+    virtual void OpenSession(const ClusterCoordinator& quorum, ExecutionContext& context) = 0; 
+	
 
-    /// directly connect to a Drill User Server if we know the address (mainly for testing)
-    virtual bool OpenSession(const UserServerEndPoint& endpoint, ExecutationContext& context) = 0;
+    /// @brief Directly connect to a Drill user server if we know the address
+	///
+	/// It will send an initial handshake request and validate the handshake.
+	///
+	/// @param[in]  quorum
+	/// @param[out] context
+    virtual void OpenSession(const UserServerEndPoint& endpoint, ExecutionContext& context) = 0;
 
-    /// Test whether the client is active
-    virtual bool Active() = 0;
+    /// @brief Send Goodbye message to close the session
+    virtual void CloseSession(ExecutionContext& context);
 
-    /// Reconnect if submission failed
-    virtual bool Reconnect() = 0;
-
-    /// Send Goodbye message to close the session
-    void CloseSession();
-
-    /// @brief Executes a SQL query in Apache Drill
+    /// @brief Prepares a SQL query and retrieve result-set meta-data from a query
+	/// 
+	/// We anticipate two classes of queries: meta-data enquiries and bona-fide queries.
     ///
     /// @param[in]  query    The query to execute. 
-    /// @param[out] context  The output context generated from executing the query.
-    /// @param[out] buffer   the output buffer
-    virtual void ExecuteStatement(const string& query, ExecutationContext& context, RowSetBuffer& buffer) = 0;
+    /// @param[out] context  The output context generated from preparing the query.
+    /// @param[out] buffer   The output buffer
+    virtual void PrepareStatement(const string& query, ExecutionContext& context, RecordBatchBuffer& buffer) = 0;
+
+    /// @brief Bind parameters values for a previously prepared SQL query 
+    ///
+    /// @param[in] context  	The context of a previously prepared query. Use query id to track the prepared query statement 
+    /// @param[in] parameter  	The list of (key,value) pairs of parameters. 
+	///							For example, could be a format like "key1:val1, key2:val2 "
+	/// @param[out] context 	The output context generated from binding statement
+    virtual void BindStatement(const ExecutionContext& in_context, const string& parameters, ExecutionContext& out_context) = 0;
+
+    /// @brief Executes a SQL query in Apache Drill. 
+	/// 
+	/// We anticipate two classes of queries: meta-data enquiries and bona-fide queries.
+    ///
+    /// @param[in]  in_context  The context of a previously prepared/bind query.
+	/// @param[out] out_context The output context generated from executing the query
+	/// @param[out] buffer   	The output buffer.
+    virtual void ExecuteStatement(const ExecutionContext& in_context, ExecutionContext& out_context, RecordBatchBuffer& buffer) = 0;
 
     /// @brief Cancel the query
-    virtual void CancelOpereation(ExecuteStatement& context) = 0;
+	///
+    virtual void CancelStatement(ExecutionContext& context) = 0;
 
-    /// @brief close an operation by sending an Ack message to the server
+    /// @brief Clean up a statement at the conclusion by sending an Ack message to the server
     /// 
     /// @question What is the expected behaviour to send an Ack before the server send all results out?
-    virtual void CloseOpereation(ExecuteStatement& context) = 0;
-    
-
-    /// @brief Fetch at most the specified number of rows for the result set.
-    ///
-    ///
-    /// @param[in] context               The context of the query to fetch for.
-    /// @param[out] buffer               The buffer to store the rows fetched.
-    /// @param[in] maxRows               The max number of rows to fetch.
-    void FetchNRows(
-        ExecutationContext& in_context,
-        RowSetBuffer& out_buffer,
-        uint32_t max_rows);
-
-
-    /// @brief Get the metadata of the result set 
-    ///
-    /// parse exec::shared::RecordBatchDef to get the result
-    ///
-    /// @param[in] context   The context of the query to get metadata for.
-    /// @param[out] columns  The column holder to write to.
-    void GetResultSetSchema(DiracExecutationContext& context, vector<string>& columns);
-
-    //  ---------------------------------------------------------------------------------------------
-    //  Use Drill information schema to get database metadata
-    //
-    //  - return results as vector of string
-    //  - some field describe in Drill Information Schema is not used (added if needed in the future)
-    //  ---------------------------------------------------------------------------------------------
-
-    /// @brief Retreive the Catalogs of the database
-    ///
-    /// @param[out] the list of catalog available on this connection.
-    void GetCatalogs(vector<string>&  catalogs);
-
-    /// @brief Retreive the Schamata of the database
-    ///
-    /// @param[out] the list of schemata available on this connection.
-    void GetSchemata(vector<string>& schemata);
-
-    /// @brief Retrieve the list of tables.
-    ///
-    /// @param[in]  schema      The schema to show tables for. This must be a valid schema.
-    /// @param[out] table_names  The vector to write the results to. Should be empty initially.
-    void GetTableNames(const string& schema, vector<string>& table_names);
-
-    /// @brief Retrieve the schema of a table.
-    ///
-    //  Query TABLES table in Drill Information Schema
-    //
-    /// @param[in]  catalog_name           The catalog name.
-    /// @param[in]  schema_name            The schema.
-    /// @param[in]  table_name             The table name.
-    /// @param[out] select_columns        The vector for storing the fetched table schema.
-    virtual void GetColumnNames(
-        const string&  catalog_cheame,
-        const string&  schema_name,
-        const string&  table_name,
-        vector<string>& select_columns);
-
-
+    virtual void EndStatement(ExecutionContext& context) = 0;
 }
 };
 
