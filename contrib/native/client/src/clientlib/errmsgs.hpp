9e885a723c41b93dcc14030e1b803247a90c2231
Parth Chandra <pchandra@maprtech.com>
2014-05-30 11:17:40 -0700

    DRILL-875: Fixes for DRILL-707, DRILL-780, DRILL-835 (Schema change), DRILL-852, DRILL-876, DRILL_877, DRILL-878, DRILL-890

--- contrib/native/client/src/clientlib/errmsgs.hpp
+++ contrib/native/client/src/clientlib/errmsgs.hpp
@@ -32,28 +32,30 @@ namespace Drill{
 #define ERR_CONN_FAILURE        1
 #define ERR_CONN_EXCEPT         2
 #define ERR_CONN_UNKPROTO       3
-#define ERR_CONN_ZOOKEEPER      4
-#define ERR_CONN_NOHSHAKE       5
-#define ERR_CONN_ZKFAIL         6
-#define ERR_CONN_ZKTIMOUT       7
-#define ERR_CONN_ZKERR          8
-#define ERR_CONN_ZKDBITERR      9
-#define ERR_CONN_ZKNODBIT       10
-#define ERR_CONN_ZKNOAUTH       11
-#define ERR_CONN_ZKEXP          12
-#define ERR_CONN_MAX            12
+#define ERR_CONN_RDFAIL         4
+#define ERR_CONN_WFAIL          5
+#define ERR_CONN_ZOOKEEPER      6
+#define ERR_CONN_NOHSHAKE       7
+#define ERR_CONN_ZKFAIL         8
+#define ERR_CONN_ZKTIMOUT       9
+#define ERR_CONN_ZKERR          10
+#define ERR_CONN_ZKDBITERR      11
+#define ERR_CONN_ZKNODBIT       12
+#define ERR_CONN_ZKNOAUTH       13
+#define ERR_CONN_ZKEXP          14
+#define ERR_CONN_MAX            14
 
-#define ERR_QRY_OUTOFMEM    ERR_CONN_MAX+1 
+#define ERR_QRY_OUTOFMEM    ERR_CONN_MAX+1
 #define ERR_QRY_COMMERR     ERR_CONN_MAX+2
 #define ERR_QRY_INVREADLEN  ERR_CONN_MAX+3
 #define ERR_QRY_INVQUERYID  ERR_CONN_MAX+4
 #define ERR_QRY_INVRPCTYPE  ERR_CONN_MAX+5
 #define ERR_QRY_OUTOFORDER  ERR_CONN_MAX+6
 #define ERR_QRY_INVRPC      ERR_CONN_MAX+7
-#define ERR_QRY_8           ERR_CONN_MAX+8
+#define ERR_QRY_TIMOUT      ERR_CONN_MAX+8
 #define ERR_QRY_FAILURE     ERR_CONN_MAX+9
 #define ERR_QRY_SELVEC2     ERR_CONN_MAX+10
-#define ERR_QRY_RESPFAIL    ERR_CONN_MAX+11 
+#define ERR_QRY_RESPFAIL    ERR_CONN_MAX+11
 #define ERR_QRY_12          ERR_CONN_MAX+12
 #define ERR_QRY_UNKQRY      ERR_CONN_MAX+13
 #define ERR_QRY_CANCELED    ERR_CONN_MAX+14
@@ -63,7 +65,7 @@ namespace Drill{
 #define ERR_QRY_18          ERR_CONN_MAX+18
 #define ERR_QRY_19          ERR_CONN_MAX+19
 #define ERR_QRY_20          ERR_CONN_MAX+20
-#define ERR_QRY_MAX         ERR_QRY_20 
+#define ERR_QRY_MAX         ERR_QRY_20
 
     // Use only Plain Old Data types in this struc. We will declare
     // a global.
